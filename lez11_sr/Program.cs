dca83ba3b34fc76032b76e46ac7bf9ac97aa3d35
Giovanni Pace <g.pace@braintechinnovations.it>
2026-05-28 10:02:16 +0200

    Sistemato Csharp

--- lez11_sr/Program.cs
+++ lez11_sr/Program.cs
@@ -6,6 +6,10 @@ builder.Services.AddDbContext<BlogContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DatabaseDocker")));
 
 builder.Services.AddScoped<PostService>();
+builder.Services.AddScoped<PostRepository>();
+builder.Services.AddScoped<CommentoRepository>();
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
